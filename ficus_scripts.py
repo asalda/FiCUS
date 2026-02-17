8da280fed26222ff8fb273bb7e0376a33156ca53
Alberto Saldana-Lopez <89804075+asalda@users.noreply.github.com>
2026-02-16 19:04:27 -0500

    Update ficus_scripts.py

--- ficus_scripts.py
+++ ficus_scripts.py
@@ -13,7 +13,6 @@
 import datetime
 import numpy as np
 import os
-import scipy as sci
 import sys
 
 from astropy import convolution
@@ -294,8 +293,8 @@ def flam_x(wave,flam,lc,wth):
     """ 
     l1_lim = lc - wth;
     l2_lim = lc + wth;
-    w_int = wave[sci.where((wave>=l1_lim)&(wave<=l2_lim))];
-    f_int = flam[sci.where((wave>=l1_lim)&(wave<=l2_lim))];
+    w_int = wave[np.where((wave>=l1_lim)&(wave<=l2_lim))];
+    f_int = flam[np.where((wave>=l1_lim)&(wave<=l2_lim))];
     flam_x = trapz(f_int,w_int) / (2*wth);
     
     return flam_x
@@ -330,7 +329,7 @@ def beta_x(wave,flam,lc1,lc2):
     flux1 = flam_x(wave,flam,lc1,50.);
     flux2 = flam_x(wave,flam,lc2,50.);
     if ((flux1*flux2) != 0.):
-        beta_x = (sci.log10(flux1)-sci.log10(flux2)) / (sci.log10(lc1)-sci.log10(lc2));
+        beta_x = (np.log10(flux1)-np.log10(flux2)) / (np.log10(lc1)-np.log10(lc2));
     else:
         beta_x = -99.;
     
@@ -345,8 +344,8 @@ def QH_IHb(wave,flam,z):
     eh = 109678.758;
     wedge_h = 1.e8 / eh;
     
-    n_int = n_lam[sci.where(wave<=wedge_h)];
-    w_int = wave[sci.where(wave<=wedge_h)];
+    n_int = n_lam[np.where(wave<=wedge_h)];
+    w_int = wave[np.where(wave<=wedge_h)];
     
     q_h  = trapz(n_int,w_int);
     IHb = 4.76e-13 * q_h;
